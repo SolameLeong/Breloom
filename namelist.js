730749fbc916753889b16753e1a5c0b8f06f578f
Solame Leong <Solame Leong>
2016-05-27 22:43:15 +0800

    initial webpage using vue.js

---
+++ namelist.js
@@ -0,0 +1,100 @@
+
+function submit(){
+
+}
+var n1 = new Vue({
+    el: '.player16_1',
+    data:{
+        num: 'H4'
+    }
+})
+var n2 = new Vue({
+    el: '.player16_2',
+    data:{
+        num: 'D5'
+    }
+})
+var n3 = new Vue({
+    el: '.player16_3',
+    data:{
+        num: 'B3'
+    }
+})
+var n4 = new Vue({
+    el: '.player16_4',
+    data:{
+        num: 'E4'
+    }
+})
+var n5 = new Vue({
+    el: '.player16_5',
+    data:{
+        num: 'F4'
+    }
+})
+var n6 = new Vue({
+    el: '.player16_6',
+    data:{
+        num: 'E1'
+    }
+})
+var n7 = new Vue({
+    el: '.player16_7',
+    data:{
+        num: 'G3'
+    }
+})
+var n8 = new Vue({
+    el: '.player16_8',
+    data:{
+        num: ''
+    }
+})
+var n9 = new Vue({
+    el: '.player16_9',
+    data:{
+        num: 'B1'
+    }
+})
+var n10 = new Vue({
+    el: '.player16_10',
+    data:{
+        num: 'H1'
+    }
+})
+var n11 = new Vue({
+    el: '.player16_11',
+    data:{
+        num: 'C3'
+    }
+})
+var n12 = new Vue({
+    el: '.player16_12',
+    data:{
+        num: 'A1'
+    }
+})
+var n13 = new Vue({
+    el: '.player16_13',
+    data:{
+        num: 'A3'
+    }
+})
+var n14 = new Vue({
+    el: '.player16_14',
+    data:{
+        num: 'D4'
+    }
+})
+var n15 = new Vue({
+    el: '.player16_15',
+    data:{
+        num: 'C4'
+    }
+})
+var n16 = new Vue({
+    el: '.player16_16',
+    data:{
+        num: 'G4'
+    }
+})
