5d932528128fda537b04c13cc47e2380500d9bdc
sunoru <s@sunoru.com>
2016-05-27 23:58:58 +0800

    guba14

---
+++ static/main.js
@@ -0,0 +1,17 @@
+(function () {
+    Vue.config.debug = true;
+    var main = new Vue({
+        el: "#main-container",
+        data: {
+            title: "第十四届菇吧淘汰赛",
+            namelist: null,
+            bracket: null
+        },
+        ready: function () {
+            this.$http.get('../data/guba14.json').then(function (response) {
+                this.namelist = response.data.namelist;
+                this.bracket = response.data.bracket;
+            })
+        }
+    });
+})();
